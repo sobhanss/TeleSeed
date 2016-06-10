8698198f75f8568b30d8d9734a2d97ee8034fbb5
sobhanss <sobiatsain@gmail.com>
2016-06-10 23:11:41 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -230,7 +230,8 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+	"plugins"
     },
     sudo_users = {148338435,0,0,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
